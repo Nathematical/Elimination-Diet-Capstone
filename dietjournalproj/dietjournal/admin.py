789578b79e43c1fffd5fc8c6a64f1d253ac246bd
Adam Meier <meieradamn@gmail.com>
2021-07-12 19:07:00 -0400

    added static folder, updates to base.html and index.html

--- dietjournalproj/dietjournal/admin.py
+++ dietjournalproj/dietjournal/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import LogEntry, Meal, WaterConsumed, Medication, BowelMovement
 
-# Register your models here.
+admin.site.register(LogEntry)
+admin.site.register(Meal)
+admin.site.register(WaterConsumed)
+admin.site.register(Medication)
+admin.site.register(BowelMovement)
--- dietjournalproj/dietjournal/models.py
+++ dietjournalproj/dietjournal/models.py
@@ -1,70 +1,61 @@
 from django.db import models
 from users.models import User
+from django.utils import timezone
 
 class LogEntry(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
-    date_created = models.DateTimeField(auto_now_add=True)
-    date_edited = models.DateTimeField(auto_now_add=True)
+    bedtime = models.DateTimeField(default=timezone.now)
+    waketime = models.DateTimeField(default=timezone.now)
+    sleep_quality = models.TextField(default='')
+    date_created = models.DateTimeField(default=timezone.now)
+    date_edited = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.date_created + '_' + self.user
 
+
 class Meal(models.Model):
+    meal_type = models.CharField(max_length=200)
     location = models.CharField(max_length=200)
     meal_num = models.IntegerField()
-    food_name = models.CharField(max_length=200)
+    food = models.CharField(max_length=200)
+    drink = models.CharField(max_length=200)
     mood = models.CharField(max_length=200)
     notes = models.TextField()
-    day_time = models.DateTimeField(auto_now=True)
+    day_time = models.DateTimeField(default=timezone.now)
     log_entry = models.ForeignKey(LogEntry, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.meal_num + '_' + self.food_name
+        return self.meal_type
+        
 
-class Snack(models.Model):
-    location = models.CharField(max_length=200)
-    snack_num = models.IntegerField()
-    food_name = models.CharField(max_length=200)
-    notes = models.TextField()
-    day_time = models.DateTimeField(auto_now=True)
-    log_entry = models.ForeignKey(LogEntry, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.snack_num + '_' + self.food_name
-
-class Sleep(models.Model):
-    bedtime = models.DateTimeField()
-    waketime = models.DateTimeField()
-    sleep_quality = models.TextField()
-    log_entry = models.ForeignKey(LogEntry, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.bedtime + '_' + self.waketime
-    
 class WaterConsumed(models.Model):
     h2o_oz = models.IntegerField()
-    day_time = models.DateTimeField(auto_now=True)
+    day_time = models.DateTimeField(default=timezone.now)
     log_entry = models.ForeignKey(LogEntry, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.h2o_oz
+
 
 class Medication(models.Model):
     name = models.CharField(max_length=200)
     reason = models.CharField(max_length=200)
     dosage = models.CharField(max_length=200)
     reaction = models.TextField()
-    day_time = models.DateTimeField(auto_now=True)
+    day_time = models.DateTimeField(default=timezone.now)
     log_entry = models.ForeignKey(LogEntry, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.dosage + '_' + self.name
+        return self.name + '_' + self.dosage
+
 
 class BowelMovement(models.Model):
     bm_num = models.IntegerField()
     notes = models.TextField()
-    day_time = models.DateTimeField(auto_now=True)
+    day_time = models.DateTimeField(default=timezone.now)
     log_entry = models.ForeignKey(LogEntry, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.bm_num + '_' + self.day_time
+        return self.bm_num
+
