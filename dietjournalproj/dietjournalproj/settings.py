7051cffff15e21f9723f42661f2e3fc3010d4e91
Adam Meier <meieradamn@gmail.com>
2021-07-14 19:39:49 -0400

    updated imports in settings.py

--- dietjournalproj/dietjournalproj/settings.py
+++ dietjournalproj/dietjournalproj/settings.py
@@ -14,8 +14,8 @@ import django_on_heroku
 from pathlib import Path
 import os
 # from . import secrets
-import dotenv
-dotenv.loadenv()
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
