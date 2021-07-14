fc757af7cb86bb1b8a0f81120addcb6a32479498
Adam Meier <meieradamn@gmail.com>
2021-07-14 19:22:10 -0400

    settings update

--- dietjournalproj/dietjournalproj/settings.py
+++ dietjournalproj/dietjournalproj/settings.py
@@ -13,7 +13,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import django_on_heroku
 from pathlib import Path
 import os
-from . import secrets
+# from . import secrets
+import dotenv
+dotenv.loadenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = secrets.DJANGO_KEY
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
